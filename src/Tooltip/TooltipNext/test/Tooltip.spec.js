439ed6daf82facf178212646b91395c1eadf7563
mykas <mykolass@wix.com>
2019-04-02 15:44:35 +0300

    <Tooltip/> - add missing semicolons

--- src/Tooltip/TooltipNext/test/Tooltip.spec.js
+++ src/Tooltip/TooltipNext/test/Tooltip.spec.js
@@ -24,7 +24,7 @@ describe('Tooltip', () => {
       const { driver } = render(
         <Tooltip onShow={onShow} content="string">
           {children}
-        </Tooltip>
+        </Tooltip>,
       );
 
       expect(await driver.getTooltipText()).toBe('string');
