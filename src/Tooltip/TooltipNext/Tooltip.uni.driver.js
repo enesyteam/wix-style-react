ff5856c438f257186cd7253558ebf867b38f1c85
mykas <mykolass@wix.com>
2019-04-03 11:13:54 +0300

    <Tooltip/> - fix unidriver testkit

--- src/Tooltip/TooltipNext/Tooltip.uni.driver.js
+++ src/Tooltip/TooltipNext/Tooltip.uni.driver.js
@@ -1,24 +1,25 @@
-import { testkit } from 'wix-ui-core/dist/src/components/Popover/Popover.uni.driver';
+import { tooltipDriverFactory as tooltipDriverFactoryCore } from 'wix-ui-core/dist/src/components/Tooltip/Tooltip.uni.driver';
 import { textUniDriverFactory } from '../../Text/Text.uni.driver';
 
 export const tooltipDriverFactory = (base, body) => {
+  const testkit = tooltipDriverFactoryCore(base, body);
   return {
     /** returns true if trigger element exists on the DOM */
-    exists: async () => await testkit(base, body).exists(),
+    exists: async () => await testkit.exists(),
     /** returns true if tooltip element exists on the DOM */
-    tooltipExists: async () => await testkit(base, body).tooltipExists(),
+    tooltipExists: async () => await testkit.tooltipExists(),
     /** mouse over the target element */
-    mouseEnter: async () => await testkit(base, body).mouseEnter(),
+    mouseEnter: async () => await testkit.mouseEnter(),
     /** mouse leaves the target element */
-    mouseLeave: async () => await testkit(base, body).mouseLeave(),
+    mouseLeave: async () => await testkit.mouseLeave(),
     /** returns tooltips content value in string */
     getTooltipText: async () => {
-      await testkit(base, body).mouseEnter();
+      await testkit.mouseEnter();
       const text = await textUniDriverFactory(
-        body.$('[data-hook="tooltip-text"]'),
+        body.$('[data-hook="tooltip-text"]')
       ).getText();
       // this makes sure that test is cleaning up after
-      await testkit(base, body).mouseLeave();
+      await testkit.mouseLeave();
       return text;
     },
   };
