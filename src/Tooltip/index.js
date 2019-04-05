2f0c4875038af51ad8b6576a6a1baf5c705f3b8d
mykas <mykolass@wix.com>
2019-04-05 11:12:44 +0300

    <Tooltip/> - experiment on upgrade prop

--- src/Tooltip/index.js
+++ src/Tooltip/index.js
@@ -5,16 +5,33 @@ import PropTypes from 'prop-types';
 import TooltipNew from './TooltipNext';
 import TooltipOld from './Tooltip';
 
-class Tooltip extends Component {
-  static propTypes = {
-    upgrade: PropTypes.bool,
-  };
-  render() {
-    const { upgrade, ...rest } = this.props;
-    return upgrade ? <TooltipNew {...rest} /> : <TooltipOld {...rest} />;
+const upgradeHOC = Old => {
+  class UpgradeTooltip extends React.Component {
+    static displayName = 'Tooltip';
+
+    static propTypes = {
+      upgrade: PropTypes.bool,
+    };
+
+    wrappedComponentRef = null;
+
+    static defaultProps = Component.defaultProps;
+
+    render() {
+      const { upgrade, ...rest } = this.props;
+      const reference = ref => (this.wrappedComponentRef = ref);
+      return upgrade ? (
+        <TooltipNew {...rest} ref={reference} />
+      ) : (
+        <Old {...rest} ref={reference} />
+      );
+    }
   }
-}
 
-Tooltip.displayName = 'Tooltip';
+  return hoistNonReactMethods(UpgradeTooltip, Old, {
+    delegateTo: c => c.wrappedComponentRef,
+    hoistStatics: true,
+  });
+};
 
-export default hoistNonReactMethods(TooltipOld, Tooltip);
+export default upgradeHOC(TooltipOld);
